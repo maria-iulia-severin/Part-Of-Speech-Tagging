4cc0e94536a6ff822ba0301d29de2561ee01a4c4
Iulia Severin <iulia.severin@ulbsibiu.ro>
2021-03-28 14:08:48 +0300

    Split Function - takes correctly all the data

--- Spatiu_de_reprezentare.cs
+++ Spatiu_de_reprezentare.cs
@@ -11,12 +11,12 @@ namespace POS_Tagging
         //ingore symbols - filter
         private bool IgnoredSymbols(string word)
         {
-            string[] ignored_symbols = { "''" , ",", ":", ".", "(", ")", "``", "?", "!", ";", "--" };
+            string[] ignored_symbols = { "''", "'", ",", ":", ".", "(", ")", "``", "?", "!", ";", "--" };
             int length = ignored_symbols.Length;
 
             for (int i = 0; i < length; i++)
             {
-                //if contains this simbole, ignore all ? pt --hl --tl
+                //if contains this symbol, ignore all ? pt --hl --tl
                 if (word == ignored_symbols[i])
                 {
                     return true;
@@ -42,6 +42,7 @@ namespace POS_Tagging
             string[] word_tag_pair; //
             char[] separators = new char[] { ' ', '\r', '\n', '\t' };
 
+            //Tag - Words in array
             foreach (string file in files)
             {
                 using (StreamReader reader = new StreamReader(file))
@@ -49,10 +50,11 @@ namespace POS_Tagging
                     string text_in_file = reader.ReadToEnd();
 
                     word_tag_pair = text_in_file.Split(separators, StringSplitOptions.RemoveEmptyEntries); //Split
-                    
-                    for(int i = 0; i < word_tag_pair.Length; i++)
+
+                    for (int i = 0; i < word_tag_pair.Length; i++)
                     {
-                        string[] word_tag_split = word_tag_pair[i].Split('/');
+                        //string[] word_tag_split = word_tag_pair[i].Split('/');
+                        string[] word_tag_split = SpitPair(word_tag_pair[i]);
                         //add words
                         AddWord(word_tag_split[0]);
                         //add tags
@@ -61,11 +63,63 @@ namespace POS_Tagging
                     }
                 }
             }
+
+            //Tag - Words in Matrix
+     /*       foreach (string file in files)
+            {
+                using (StreamReader reader = new StreamReader(file))
+                {
+                    string text_in_file = reader.ReadToEnd();
+
+                    word_tag_pair = text_in_file.Split(separators, StringSplitOptions.RemoveEmptyEntries); //Split
+
+                    for (int i = 0; i < word_tag_pair.Length; i++)
+                    {
+                        //string[] word_tag_split = word_tag_pair[i].Split('/');
+                        string[] word_tag_split = SpitPair(word_tag_pair[i]);
+                        //add words
+                        AddWord(word_tag_split[0]);
+                        //add tags
+                        AddTag(word_tag_split[1]);
+
+                    }
+                }
+            }*/
+
+            for (int i = 0; i < tag_array.Length; i++)
+            {
+                Console.WriteLine(tag_array[i]);
+            }
+        }
+
+        private static string[] SpitPair(string word_tag_pair)
+        {
+
+            string[] word_tag_return = new string[2];
+            char[] test = word_tag_pair.ToCharArray();
+
+            int length = test.Length - 1;
+            int i = length;
+
+            char[] tag = new char[20];
+
+            while (test[i] != '/')
+            {
+                tag[length - i] = test[i];
+                i--;
+            }
+
+            word_tag_return[0] = word_tag_pair.Substring(0, i);
+            Array.Reverse(tag);
+            word_tag_return[1] = new string(tag).Trim('\0');
+
+            return word_tag_return;
         }
 
         private void AddWord(string word)
         {
             bool exists_in_array = false;
+            //char[] charsToTrim = { '\'', '$', ' ', '.', '-' };
             for (int j = 0; j < word_count; j++)
             {
                 if (word_array[j] == word)
@@ -74,8 +128,11 @@ namespace POS_Tagging
                     break;
                 }
             }
-            if (!exists_in_array && !IgnoredSymbols(word))
+            if (!exists_in_array)
+            // && !IgnoredSymbols(word))
             {
+
+                //string word_trim = word.TrimEnd('\'');
                 word_array[word_count] = word;
                 word_count++;
             }
@@ -92,7 +149,8 @@ namespace POS_Tagging
                     break;
                 }
             }
-            if (!exists_in_array && !IgnoredSymbols(tag))
+            if (!exists_in_array)
+            // && !IgnoredSymbols(tag))
             {
                 tag_array[tag_count] = tag;
                 tag_count++;
