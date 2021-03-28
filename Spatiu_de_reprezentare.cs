241b789fae34dcdb1e1a2498a410eae0096d01b2
Iulia Severin <iulia.severin@ulbsibiu.ro>
2021-03-28 19:31:00 +0300

    GetNounFrequence & Predict Function

--- Spatiu_de_reprezentare.cs
+++ Spatiu_de_reprezentare.cs
@@ -5,9 +5,17 @@ using System.Windows.Forms;
 
 namespace POS_Tagging
 {
-
     public partial class Spatiu_de_reprezentare : Form
     {
+        string[] noun = { "nn", "nn$", "nns", "nns$", "np", "np$", "nps", "nps$", "nrs" };
+        string[] verb = { " " };
+        string[] adjective = { " " };
+        string[] adverb = { " " };
+        string[] pronoun = { " " };
+        string[] conjunction = { " " };
+        string[] article = { " " };
+        string[] preposition = { " " };
+        string[] other = { " " };
         //ingore symbols - filter
         private bool IgnoredSymbols(string word)
         {
@@ -29,19 +37,22 @@ namespace POS_Tagging
         {
             InitializeComponent();
             ReadCorpus();
+            Console.WriteLine(Predict("shelter"));
         }
 
         string[] word_array = new string[60000];
         string[] tag_array = new string[600];
+        int[,] matrix = new int[60000, 600];
         int word_count = 0;
         int tag_count = 0;
+        int count_words = 0;
         public void ReadCorpus()
         {
             string rootPath = @"C:\Users\iulia.severin\source\repos\POS-Tagging\bin\Debug\brown";
             var files = Directory.GetFiles(rootPath, "*.*", SearchOption.AllDirectories);
             string[] word_tag_pair; //
             char[] separators = new char[] { ' ', '\r', '\n', '\t' };
-            int count_words = 0;
+
             //Tag - Words in array
             foreach (string file in files)
             {
@@ -55,17 +66,21 @@ namespace POS_Tagging
                     for (int i = 0; i < word_tag_pair.Length; i++)
                     {
                         //string[] word_tag_split = word_tag_pair[i].Split('/');
-                        string[] word_tag_split = SpitPair(word_tag_pair[i]);
+                        WordTags word_tag_split = SpitPair(word_tag_pair[i]);
+
                         //add words
-                        AddWord(word_tag_split[0]);
-                        //add tags
-                        AddTag(word_tag_split[1]);
+                        AddWord(word_tag_split.word);
+                        for (int j = 0; j < word_tag_split.tags.Count; j++)
+                        {
+                            //add tags
+                            AddTag(word_tag_split.tags[j]);
+                        }
 
                     }
                 }
             }
             Console.WriteLine(count_words);
-            int[,] matrix = new int[word_array.Length, tag_array.Length];
+
 
             //Tag - Words in Matrix
             foreach (string file in files)
@@ -79,26 +94,79 @@ namespace POS_Tagging
                     for (int i = 0; i < word_tag_pair.Length; i++)
                     {
                         //string[] word_tag_split = word_tag_pair[i].Split('/');
-                        string[] word_tag_split = SpitPair(word_tag_pair[i]);
-                        matrix[GetWordPosition(word_tag_split[0]), GetTagPosition(word_tag_split[1])]++;
+                        WordTags word_tag_split = SpitPair(word_tag_pair[i]);
+                        for (int j = 0; j < word_tag_split.tags.Count; j++)
+                        {
+                            matrix[GetWordPosition(word_tag_split.word), GetTagPosition(word_tag_split.tags[j])]++;
+                        }
                     }
                 }
             }
-/*
+
+
+            Console.WriteLine(GetNounFrequence());
+            /* for (int i = 0; i < word_array.Length; i++)
+             {
+                 for (int j = 0; j < tag_array.Length; j++)
+                 {
+                     Console.Write(matrix[i, j] + " ");
+                 }
+                 Console.Write("\n");
+             }
+             for (int i = 0; i < tag_array.Length; i++)
+             {
+                 Console.WriteLine(tag_array[i]);
+             }*/
+        }
+        private string Predict(string word)
+        {
+            int wordPosition = GetWordPosition(word);
+            int maxFrequence = -1;
+            int maxFrequencePosition = 0;
+            for (int i = 0; i < tag_array.Length; i++)
+            {
+                if (matrix[wordPosition, i] > maxFrequence)
+                {
+                    maxFrequence = matrix[wordPosition, i];
+                    maxFrequencePosition = i;
+                }
+            }
+            return tag_array[maxFrequencePosition];
+        }
+        private double GetNounFrequence()
+        {
+            int[] nounPositions = GetNounIndexes();
+            int nounsSum = 0;
             for (int i = 0; i < word_array.Length; i++)
             {
-                for (int j = 0; j < tag_array.Length; j++)
-                { 
-                    Console.Write(matrix[i,j] + " "); 
-
+                for (int j = 0; j < nounPositions.Length; j++)
+                {
+                    nounsSum += matrix[i, nounPositions[j]];
                 }
-                Console.Write("\n");
-            }*/
-            /*     for (int i = 0; i < tag_array.Length; i++)
-                 {
-                     Console.WriteLine(tag_array[i]);
-                 }*/
+            }
+            Console.WriteLine(nounsSum);
+            Console.WriteLine(count_words);
+            return 1.0 * nounsSum / count_words;
         }
+
+        private int[] GetNounIndexes()
+        {
+            int[] positions = new int[noun.Length];
+
+            for (int i = 0; i < noun.Length; i++)
+            {
+                for (int j = 0; j < tag_array.Length; j++)
+                {
+                    if (noun[i] == tag_array[j])
+                    {
+                        positions[i] = j;
+                        break;
+                    }
+                }
+            }
+            return positions;
+        }
+
         private int GetTagPosition(string tag)
         {
             for (int i = 0; i < tag_array.Length; i++)
@@ -121,30 +189,31 @@ namespace POS_Tagging
             }
             return -1;
         }
-        private static string[] SpitPair(string word_tag_pair)
+        private static WordTags SpitPair(string word_tag_pair)
         {
 
-            string[] word_tag_return = new string[2];
-            char[] test = word_tag_pair.ToCharArray();
+            //string[] word_tag_return = new string[2];
+            WordTags word_tag_return = new WordTags();
+            string tagGroup;
+            char[] charArray = word_tag_pair.ToCharArray();
 
-            int length = test.Length - 1;
-            int i = length;
-
+            int charLength = charArray.Length - 1;
+            int i = charLength;
             char[] tag = new char[20];
 
-            while (test[i] != '/')
+            while (charArray[i] != '/')
             {
-                tag[length - i] = test[i];
+                tag[charLength - i] = charArray[i];
                 i--;
             }
 
-            word_tag_return[0] = word_tag_pair.Substring(0, i);
+            word_tag_return.word = word_tag_pair.Substring(0, i);
             Array.Reverse(tag);
-            word_tag_return[1] = new string(tag).Trim('\0');
+            tagGroup = new string(tag).Trim('\0');
+            word_tag_return.tags = new List<string>(tagGroup.Split(new char[] { '-', '+', '*', ' ' }, StringSplitOptions.RemoveEmptyEntries));
 
             return word_tag_return;
         }
-
         private void AddWord(string word)
         {
             bool exists_in_array = false;
---
+++ WordTags.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POS_Tagging
+{
+    class WordTags
+    {
+        public string word;
+        public List<string> tags;
+    }
+}
